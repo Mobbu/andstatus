6f8bb05a23341769cee69f126aac0fcd173a1f6a
Ollie Edwards <olliee@mobbu.com>
2015-11-26 14:42:26 +0000

    269 - xml in right dir, remove onCreateImpl, default XML strategy is fine for now

--- app/src/main/java/org/andstatus/app/AuthActivity.java
+++ app/src/main/java/org/andstatus/app/AuthActivity.java
@@ -1,7 +1,12 @@
 package org.andstatus.app;
 
 import android.content.Intent;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
 import com.mobbu.passwear.api.PasswearActivity;
+import com.mobbu.passwear.api.PasswearXmlConfig;
+
 import org.andstatus.app.msg.TimelineActivity;
 
 /**
